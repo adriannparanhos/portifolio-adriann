d73f42d57050b8f7a2d179b1c12c610b369d76c4
Adriann Paranhos <adriannparanhos@gmail.com>
2026-02-07 19:39:37 -0300

    Feat: Added skills to the about section. Adjusted style and flexbox using BEM

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
         loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent)
     },
     {
+        path: 'about',
+        loadComponent: () => import('./features/about/about.component').then(m => m.AboutComponent)
+    },
+    {
         path: 'projects',
         loadComponent: () => import('./features/projects/projects.component').then(m => m.ProjectsComponent)
     },
